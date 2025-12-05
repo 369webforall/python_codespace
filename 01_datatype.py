3c22fcb81e9d7b99bd7a3e46c3844c610308583b
369webforall <114460098+369webforall@users.noreply.github.com>
2025-12-05 06:41:13 +0000

    python project setup

---
+++ 01_datatype.py
@@ -0,0 +1,30 @@
+print("hello world")
+
+x = 10
+
+y = 15
+
+print(x + y)
+
+# data type (int, float)
+
+x = 20
+
+print(id(x))
+
+x = 30
+
+print(id(x))
+
+
+nums = [5,6,8,9]
+
+print(type(nums))
+
+print(id(nums))
+
+nums.append(100)
+
+print(nums)
+
+print(id(nums))
